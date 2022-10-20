14c79f948963233ec760d66e0da6fe375208e02c
Lisa Meyer <lisamermey@gmail.com>
2022-10-20 17:45:39 -0400

    Define author resolver

--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -5,7 +5,11 @@ const resolvers = {
     tracksForHome: (_, __, { dataSources }) => {
       return dataSources.trackAPI.getTracksForHome();
     }
-  },
+  }, Track: {
+    author: ({ authorId }, _, { dataSources }) => {
+      return dataSources.trackAPI.getAuthor(authorId);
+    }
+  }
 };
 
 module.exports = resolvers;
