d2a26445e090e6d8637ea96cfe4bde5a5871cd50
Lisa Meyer <lisamermey@gmail.com>
2022-10-20 15:46:48 -0400

    Retrieves track data

--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -3,9 +3,9 @@ const resolvers = {
     // returns an array of Tracks that will be used to populate
     // the homepage grid of the web client
     tracksForHome: (_, __, { dataSources }) => {
-
+      return dataSources.trackAPI.getTracksForHome();
     }
-  }
+  },
 };
 
 module.exports = resolvers;
